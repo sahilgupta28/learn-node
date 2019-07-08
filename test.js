f2a1d61ad0ddab52598061d7ac91177779867be3
Sahil <er.sahilgupta1@gmail.com>
2019-07-08 22:23:19 +0530

    server setup

--- test.js
+++ test.js
@@ -1,6 +1,13 @@
 const {sum} = require("./helper")
+const http = require("http")
 
-const total = sum(10, 300)
+const server = http.createServer((req, res) => {
+	res.end("Hello world");
+});
+
+server.listen(3000);
+
+const total = sum(10, 30)
 console.log(total);
 // console.log(helper.welcome())
 // console.log(helper.areaOfSquare(10))
