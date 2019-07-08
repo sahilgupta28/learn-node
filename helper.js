f0b583963455ed831bff04f248657040b4f8f8c1
Sahil <er.sahilgupta1@gmail.com>
2019-07-08 21:32:12 +0530

    without parameter function

--- helper.js
+++ helper.js
@@ -6,6 +6,10 @@ const sum = (a, b) => {
 	return a + b;
 }
 
+const welcome = () => {
+	return 'Welcoe to nodejs';
+}
+
 module.exports={
-	sum
+	sum, welcome
 }
--- test.js
+++ test.js
@@ -2,3 +2,4 @@ const helper = require("./helper")
 
 const total = helper.sum(10, 300)
 console.log(total);
+console.log(helper.welcome())
