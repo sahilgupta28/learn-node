78d7e3bbdbe63b2d80bf6434e31213e58aa967d3
Sahil <er.sahilgupta1@gmail.com>
2019-07-09 22:34:22 +0530

    Add middleware

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
+
 
 const {getPosts} = require('./routes/post')
+
+app.use(morgan("dev"))
+
+const middleware =(req, res, next) => {
+	console.log("My middleware.");
+	next();
+}
+
+app.use(middleware)
 
 app.get("/", getPosts);
 const port = 3000;
