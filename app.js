eaf3457e953b4accbd13be9898c443b36c44106b
Sahil <er.sahilgupta1@gmail.com>
2019-07-09 22:16:53 +0530

    make api in node

--- app.js
+++ app.js
@@ -1,22 +1,12 @@
-// const express = require("express");
-// const app = express();
+const express = require("express");
+const app = express();
 
-// app.get("/", (req, res) => {
-// 	res.send("hey express");
-// });
-// app.listen(3000);
+app.get("/", (req, res) => {
+	res.send("hey express");
+});
+const port = 3000;
 
-const fs = require('fs')
-const file_name = 'target.txt'
+app.listen(port, () => {
+	console.log(`Node working on port : ${port}`);
+});
 
-const errHandler = err => console.log(err);
-const dataHandler = data => console.log(data.toString());
-
-fs.readFile(file_name, (err, data) => {
-	if(err){
-		errHandler(err);
-	}
-	dataHandler(data);
-})
-
-console.log('Async programming...?');
